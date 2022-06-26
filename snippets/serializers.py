78455fde0f9193ddf6af3289292980b54e157191
cheezedoodles <hougvapes@gmail.com>
2022-06-26 18:12:26 +0300

    minor snippet serializer update

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -13,6 +13,9 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    
+
     class Meta:
         model = Snippet
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+        fields = ['id', 'title', 'owner', 'code', 'linenos', 'language', 'style']
